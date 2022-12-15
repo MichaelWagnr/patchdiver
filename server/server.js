3c3a795ecba6521a7c2185e8074369fec27d04d9
Michael Wagner <michaelwagner780@gmail.com>
2022-12-15 09:50:59 -0500

    increase api limit

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const { authorize } = require('./utils/authorize')
 const port = 8000
 
 const limiter = rateLimit({
-	max: 250,
+	max: 777,
 	windowMs: 60 * 60 * 1000,
 	message: 'Too many requests from this IP, please try again in an hour.',
 })
