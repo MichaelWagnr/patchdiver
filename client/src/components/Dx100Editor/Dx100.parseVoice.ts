74cb24c5bc9067e2458e219dee1086671fdb5a94
Michael Wagner <michaelwagner780@gmail.com>
2023-02-09 17:39:37 -0500

    refactor parseVoice function

--- client/src/components/Dx100Editor/Dx100.parseVoice.ts
+++ client/src/components/Dx100Editor/Dx100.parseVoice.ts
@@ -2,16 +2,28 @@ import { DX100Patch, DX100SysexArr } from '../../types'
 import { dx100Parameters } from './Dx100.paramData'
 
 const parseVoice = (sysexArr: DX100SysexArr): DX100Patch | {} => {
-	const newArr = sysexArr.slice(6, -1)
-	const keys = Object.keys(dx100Parameters)
-	const voice: { [key: string]: { param: number; value: number } } = {}
+	const paramsArr = removeHeaders(sysexArr)
+	const patch = createPatchObject(paramsArr)
+	return patch
+}
+
+function removeHeaders(sysexArr: DX100SysexArr): number[] {
+	return sysexArr.slice(6, -1)
+}
+
+function createPatchObject(paramsArr: number[]) {
+	const keys = Object.keys(dx100Parameters) // ["op4_ar", "op4_d1r", ...]
+
+	const patchObject: { [key: string]: { param: number; value: number } } = {}
+
 	keys.forEach((key, index) => {
-		voice[key] = {
+		patchObject[key] = {
 			param: dx100Parameters[key].param,
-			value: newArr[index],
+			value: paramsArr[index],
 		}
 	})
-	return voice
+
+	return patchObject
 }
 
 export default parseVoice
