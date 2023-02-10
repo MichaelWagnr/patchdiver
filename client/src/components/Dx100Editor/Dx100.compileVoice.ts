0e683bb7f6dec40b5f1bda7bb6cf18cc352b4e2c
Michael Wagner <michaelwagner780@gmail.com>
2023-02-10 14:26:53 -0500

    refactor compileVoice function

--- client/src/components/Dx100Editor/Dx100.compileVoice.ts
+++ client/src/components/Dx100Editor/Dx100.compileVoice.ts
@@ -1,25 +1,28 @@
 import { DX100Patch } from '../../types'
 
 const compileVoice = (voiceObj: DX100Patch): number[] => {
-	const calculateChecksum = (arr: number[]): number => {
-		let sum = 0
-		arr.forEach((val) => (sum += val))
-		const checksum = 128 - (sum % 128)
-		return checksum === 128 ? 0 : checksum
-	}
-
-	const sysexArr = [240, 67, 0, 3, 0, 93]
-
-	const objArr = Object.values(voiceObj)
-	const sorted = objArr.sort((a, b) => a.param - b.param)
-	const arrValues = sorted.map((obj) => obj.value)
-	const arrMinusOldChecksum = arrValues.slice(0, -1)
-	const newCheckSum = calculateChecksum(arrMinusOldChecksum)
-
-	arrMinusOldChecksum.forEach((value) => sysexArr.push(value))
-	sysexArr.push(newCheckSum)
-	sysexArr.push(247)
+	const values = getArrOfValues(voiceObj)
+	const checksum = calculateChecksum(values)
+	const sysexArr = [240, 67, 0, 3, 0, 93, ...values, checksum, 247]
 	return sysexArr
+}
+
+function getArrOfValues(voiceObj: DX100Patch): number[] {
+	const arrOfNestedObjects = Object.values(voiceObj)
+	const sortedArrOfObjects = arrOfNestedObjects.sort(
+		(a, b) => a.param - b.param
+	)
+	const arrOfValuesMinusChecksum = sortedArrOfObjects
+		.map((obj) => obj.value)
+		.slice(0, -1)
+	return arrOfValuesMinusChecksum
+}
+
+function calculateChecksum(arrOfValues: number[]): number {
+	let sum = 0
+	arrOfValues.forEach((val) => (sum += val))
+	const checksum = 128 - (sum % 128)
+	return checksum === 128 ? 0 : checksum
 }
 
 export default compileVoice
