a99380ace501a6bfe11e469cc997a59ab53b61b3
Michael Wagner <michaelwagner780@gmail.com>
2023-02-11 15:50:03 -0500

    add types to Provider component

--- client/src/contexts/Provider.tsx
+++ client/src/contexts/Provider.tsx
@@ -2,7 +2,7 @@ import MidiProvider from './MidiContext'
 import PatchProvider from './PatchContext'
 import UserProvider from './UserContext'
 
-const Provider = ({ children }) => {
+const Provider = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<UserProvider>
 			<MidiProvider>
