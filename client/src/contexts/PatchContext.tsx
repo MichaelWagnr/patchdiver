0ac718bf9036a1275b222bf3aa0089b5d0db6357
Michael Wagner <michaelwagner780@gmail.com>
2023-02-11 15:49:02 -0500

    add types to PatchContext

--- client/src/contexts/PatchContext.tsx
+++ client/src/contexts/PatchContext.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
 import { createContext } from 'react'
 
 export const PatchContext = createContext(null)
 
-const PatchProvider = ({ children }) => {
+const PatchProvider = ({ children }: { children: ReactNode }) => {
 	// Patches are user-created in editor
 	const [patch, setPatch] = useState(null)
 	// Voices are either initial or from preset
 	const [voice, setVoice] = useState(null)
 	const [currentChanges, setCurrentChanges] = useState([null, null])
 
-	const handlePatchChange = (id, name, key, val) => {
+	const handlePatchChange = (id: string, name: string, val: number): void => {
+		console.log(id, name, val)
 		setCurrentChanges([name, val])
 		setPatch({ ...patch, [id]: { ...patch[id], value: val } })
 	}
